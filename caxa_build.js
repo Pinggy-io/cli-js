d90c86b25c66e020926567ab94269bc7c86d2de2
Niladri2003 <niladri.adak.21@aot.edu.in>
2025-12-04 19:59:19 +0530

    refactor:  caxa_build improved to include platform & arch

--- caxa_build.js
+++ caxa_build.js
@@ -1,9 +1,18 @@
 import caxa from 'caxa';
 
 (async () => {
+
+  const platform = process.platform;      // win32, linux, darwin
+  const arch = process.arch;              // x64, arm64, ia32
+  
+  const extension =
+    platform === "win32" ? ".exe" :
+    platform === "darwin" ? "" : "";
+
   await caxa({
     input: "./",
-    output: "bin/pinggy",
+    output: `bin/pinggy-${platform}-${arch}${extension}`,
+    includeNode: true,
     command: [
       "{{caxa}}/node_modules/.bin/node",
       "{{caxa}}/dist/index.js",
