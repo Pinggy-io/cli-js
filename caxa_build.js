7664ddcbf88a6bc45a3cb178f490f4c888bad46f
Niladri2003 <niladri.adak.21@aot.edu.in>
2025-12-10 13:13:45 +0530

    fix: implement  GitHub Actions workflow for building and uploading binaries to github release

--- caxa_build.js
+++ caxa_build.js
@@ -1,9 +1,12 @@
 import caxa from 'caxa';
+import { readFileSync } from 'fs';
 
 (async () => {
-
   const platform = process.platform;      // win32, linux, darwin
   const arch = process.arch;              // x64, arm64, ia32
+  
+  const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'));
+  const version = packageJson.version;
   
   const extension =
     platform === "win32" ? ".exe" :
@@ -11,7 +14,7 @@ import caxa from 'caxa';
 
   await caxa({
     input: "./",
-    output: `bin/pinggy-${platform}-${arch}${extension}`,
+    output: `bin/pinggy-${version}-${platform}-${arch}${extension}`,
     includeNode: true,
     command: [
       "{{caxa}}/node_modules/.bin/node",
